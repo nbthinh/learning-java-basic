df7171a67e058511907f4377b1da44357caf7dcb
Nguyễn Bửu Thịnh <nguyenbuuthinh@Nguyens-MacBook-Pro.local>
2025-11-07 13:57:29 +0700

    first commit

---
+++ src/hoidanit/HelloWorld.java
@@ -0,0 +1,10 @@
+package hoidanit;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello world from class");
+		System.out.println("Hello world from class");
+		System.out.println("Hello world from class");
+		System.out.println("Hello world from class");
+	}
+}
