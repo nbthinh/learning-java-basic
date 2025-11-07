700a3d2b1fb7e31137f09dd76d4229345636d490
Nguyễn Bửu Thịnh <nguyenbuuthinh@Nguyens-MacBook-Pro.local>
2025-11-07 15:22:56 +0700

    ham2

--- src/hoidanit/HelloWorld.java
+++ src/hoidanit/HelloWorld.java
@@ -5,11 +5,18 @@ import java.util.Scanner;
 public class HelloWorld {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Your name is: ");
-		String name = scanner.nextLine();
-		System.out.println("Your age is: ");
-		int age = scanner.nextInt();
-		System.out.println("My name is " + name + ", my age is " + age);
-		scanner.close();
+		System.out.println("First Number: " + Math.sqrt(49));
+		int firstNumber = scanner.nextInt();
+		System.out.println("Second Number: ");
+		int secondNumber = scanner.nextInt();
+		System.out.println("Max = " + Math.min(firstNumber, secondNumber));
+
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("Your name is: ");
+//		String name = scanner.nextLine();
+//		System.out.println("Your age is: ");
+//		int age = scanner.nextInt();
+//		System.out.println("My name is " + name + ", my age is " + age);
+//		scanner.close();
 	}
 }
