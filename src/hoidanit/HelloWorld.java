556e1e9e6601e3f08f19bbbe1523b35dd7ebc81c
Nguyễn Bửu Thịnh <nguyenbuuthinh@Nguyens-MacBook-Pro.local>
2025-11-07 14:58:26 +0700

    Cau lenh co ban

--- src/hoidanit/HelloWorld.java
+++ src/hoidanit/HelloWorld.java
@@ -1,10 +1,15 @@
 package hoidanit;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello world from class");
-		System.out.println("Hello world from class");
-		System.out.println("Hello world from class");
-		System.out.println("Hello world from class");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Your name is: ");
+		String name = scanner.nextLine();
+		System.out.println("Your age is: ");
+		int age = scanner.nextInt();
+		System.out.println("My name is " + name + ", my age is " + age);
+		scanner.close();
 	}
 }
