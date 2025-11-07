d91591aade2a7b7974c7926d7c9fcea67cee64a6
Nguyễn Bửu Thịnh <nguyenbuuthinh@Nguyens-MacBook-Pro.local>
2025-11-07 15:56:30 +0700

    1

--- src/hoidanit/Bai01.java
+++ src/hoidanit/Bai01.java
@@ -5,9 +5,11 @@ import java.util.Scanner;
 public class Bai01 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Nhap ho ten: ");
+		System.out.println("Ten sinh vien: ");
 		String hoten = scanner.nextLine();
-		System.out.println("Your name is: " + hoten);
+		System.out.println("Nhap diem: ");
+		int diem = scanner.nextInt();
+		System.out.println("Your name is: " + hoten + ", co diem la: " + diem);
 		scanner.close();
 	}
 }
