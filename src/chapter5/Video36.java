943d6ddc6dc8d2b4df77ad30d42b93b28b31a605
Nguyễn Bửu Thịnh <nguyenbuuthinh@Nguyens-MacBook-Pro.local>
2025-11-10 22:45:07 +0700

    12

--- src/chapter5/Video36.java
+++ src/chapter5/Video36.java
@@ -11,9 +11,19 @@ public class Video36 {
 			System.out.print("Nhap vao 1 so thuc bat ky");
 			double input = scanner.nextDouble();
 			arrNumber.add(input);
+			scanner.nextLine();
 			System.out.print("Continue ? Y or N");
-
+			String option = scanner.nextLine();
+			if (option.equals("N") || option.equals("n")) {
+				break;
+			}
 		}
+		System.out.println("Check array = " + arrNumber);
+		double sum = 0;
+		for (int i = 0; i < arrNumber.size(); i++) {
+			sum += arrNumber.get(i);
+		}
+		System.out.println("Check sum = " + sum);
 		scanner.close();
 	}
 }
